0ffd2d6e8a73c1dc7313d89fad16c8083e201e91
root <younesHassan>
2022-09-08 14:55:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
-* Description: main - Prints out a string to stdout.
+
+ * Description: main - Prints out a string to stdout.
 
  * Return: 0 if success.
 
  */
+
 int main(void)
 
 {
