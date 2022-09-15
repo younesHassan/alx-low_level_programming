1adc7ba067a8dd19edbe02d82798cd68b30dbeb6
root <younesHassan>
2022-09-15 06:12:16 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,19 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 #include <stdio.h>
 
 
 
 /**
-
  * main - prints the numbers from 1 to 100, followed by a new line
-
  * but for multiples of three prints Fizz instead of the number
-
  * and for the multiples of five prints Buzz
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
@@ -68,3 +63,4 @@ int main(void)
 
 	return (0);
 
+}
