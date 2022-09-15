18d424cfb8733b8c0dbe2897bdedcc16fa43a09b
root <younesHassan>
2022-09-15 09:32:38 -0700

    noha

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,9 +5,7 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 
 	long int n;
@@ -21,35 +19,20 @@ int main(void)
 	max = -1;
 
 	while (n % 2 == 0)
-
 	{
-
 		max = 2;
-
 		n /= 2;
 	}
-
 	for (i = 3; i <= sqrt(n); i = i + 2)
-
 	{
-
 		while (n % i == 0)
-
 		{
-
 			max = i;
-
 			n = n / i;
 		}
-
 	}
-
 	if (n > 2)
-
 		max = n;
-
 	printf("%ld\n", max);
-
 	return (0);
-
 }
