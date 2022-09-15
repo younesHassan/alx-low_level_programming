7b1283efdb8135433b0940a46ceb51468e235d66
root <younesHassan>
2022-09-15 07:40:11 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
-#ifndef main.h
+#ifndef MAIN_H
 
-#define main.h
+#define MAIN_H
 
 
 
@@ -10,12 +10,6 @@
 
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c);
-
-int convert_day(int month, int day);
-
-void print_remaining_days(int month, int day, int year);
 
 
-
-#endif /* main.h */
+#endif /* MAIN_H */
