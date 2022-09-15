d914405bf9e5554f36d9e910260bfdec9323e9eb
root <younesHassan>
2022-09-15 05:16:07 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,9 @@
 
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4, followed by a new line
- */
+* print_most_numbers - prints the numbers, from 0 to 9,
+* except 2 and 4, followed by a new line
+*/
 
 
 
