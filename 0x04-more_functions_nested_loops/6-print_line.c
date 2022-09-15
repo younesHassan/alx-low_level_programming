87eae762fc7dc22442360455f8b41651cf756552
root <younesHassan>
2022-09-15 05:52:34 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,8 @@
 
 
 /**
-
  * print_line - draws a straight line in the terminal
-
  * @n: number of times the character _ should be printed
-
  */
 
 
@@ -76,3 +73,4 @@ void print_line(int n)
 
 
 
+}
