782f42e3c6c457d2e49d70fb751a75fcd96667ec
younesHassan <105860222+younesHassan@users.noreply.github.com>
2022-09-16 00:47:09 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,36 @@
-void print_string_2(char *str);
-int main(void)
-{
-  char *str;
-  str = "0123456789\n";
-  print_string_2(str);
-  return (0);
-}
+int _putchar(char c);
+
+
+void print_alphabet(void);
+
+
+void print_alphabet_x10(void);
+
+
+int _islower(int c);
+
+
+int _isalpha(int c);
+
+
+int print_sign(int n);
+
+
+int _abs(int n);
+
+
+int print_last_digit(int n);
+
+
+void jack_bauer(void);
+
+
+void times_table(void);
+
+
+int add(int a, int b);
+
+
+void print_to_98(int n);
+
+
