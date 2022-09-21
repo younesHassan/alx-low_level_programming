5f27ad053677022c823662c0f18d8895590c6c85
younesHassan <105860222+younesHassan@users.noreply.github.com>
2022-09-21 21:47:24 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,39 @@
-#include "holberton.h"
-#include <stdio.h>
-
+#include "main.h"
 /**
-  * cap_string - ...
-  * @s: ...
-  *
-  * Return: char value
-  */
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+
+
 char *cap_string(char *s)
+
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
-	while (s[a])
+	int count = 0, i;
+
+
+
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+
+
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		i = 0;
-
-		while (i < cspc)
+		for (i = 0; i < 13; i++)
 		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] -= 32;
-
-			i++;
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
 		}
-
-		a++;
+		count++;
 	}
-
 	return (s);
 }
