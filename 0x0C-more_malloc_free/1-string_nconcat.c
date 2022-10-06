e86d2502df47fcca22124e3d572c92d8acade50b
younesHassan <105860222+younesHassan@users.noreply.github.com>
2022-10-06 17:50:15 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,40 @@
-#include <stdlib.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
-* _strlen - calculate and return string length
-* @string: string
-* Return: string length
-*/
-int _strlen(char *string)
-{
-int i;
-for (i = 0; string[i] != '\0'; i++)
-;
-return (i);
-}
-/**
-* string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
-* @s1: string 1
-* @s2: string 2
-* @n: n bytes to concat from string 2
-* Return: pointer to concatenated string
-*/
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *ptr;
-int num, len, i, j;
-num = n;
-if (s1 == NULL) 
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-if (num < 0)
-return (NULL);
-if (num >= _strlen(s2)) 
-num = _strlen(s2);
-len = _strlen(s1) + num + 1; 
-ptr = malloc(sizeof(*ptr) * len); 
-if (ptr == NULL)
-return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-ptr[i] = s1[i];
-for (j = 0; j < num; j++)
-ptr[i + j] = s2[j];
-ptr[i + j] = '\0';
-return (ptr);
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+	if (n > ls2)
+		n = ls2;
+	lsout = ls1 + n;
+	sout = malloc(lsout + 1);
+	if (sout == NULL)
+		return (NULL);
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+	sout[i] = '\0';
+	return (sout);
 }
+
